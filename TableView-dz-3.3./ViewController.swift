584a58b658249bf0fed4b4f95bb649431e8b00ac
Ainur <ismy1286@gmail.com>
2021-11-25 14:55:41 +0600

    Initial Commit

---
+++ TableView-dz-3.3./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView-dz-3.3.
+//
+//  Created by user on 25.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
